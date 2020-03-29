b1b347e6c8fb20dd28b234d8f50ba2dd40dfc764
Austin Meyer <frizzkitten@gmail.com>
2020-03-29 14:03:11 -0500

    pretty equations, pretty expander button

---
+++ src/components/icons/DownCaret.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default props => (
+    <svg height={512} viewBox="0 0 512 512" width={512} {...props}>
+        <path d="M396.6 160l19.4 20.7L256 352 96 180.7l19.3-20.7L256 310.5z" />
+    </svg>
+);
--- src/pages/results.js
+++ src/pages/results.js
@@ -8,18 +8,20 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ListGroup from "react-bootstrap/ListGroup";
 import Button from "react-bootstrap/Button";
+import DownCaret from "../components/icons/DownCaret";
 
 import axios from "axios";
+import MathJax from "react-mathjax2";
 
 const DEFAULT_EQUATIONS = [
     {
-        equation: "Fw = 0.5*p*v^2*A",
+        equation: "F_w = 0.5pv^2A",
         confidence: 100,
         description:
             "Wind force is like totally legit bro, like honestly I don't even know what to say. Like, fo real. Hot diggity schlongus up in this higgity hizzoussee bro! Anyway here's my number, hit me up sometime if you wanna listen to Linkin Park and drink mountain dew and play Halo, it's lit. Bro, I'm serious, bro."
     },
     {
-        equation: "Eq2 = schliggity kringle",
+        equation: "p(v) = (4v^(2))/(pi^(1/2))(m/(2kT))^(3/2)e^(-(mv^2)/(2kT))",
         confidence: 80,
         description: "yuh"
     },
@@ -78,47 +80,58 @@ class Results extends React.Component {
                         </Col>
                     </Row>
 
-                    <ListGroup variant="flush">
-                        {this.state.equations.map((eq, eq_index) => (
-                            <ListGroup.Item
-                                key={eq.equation}
-                                className="equation-list-item"
-                            >
-                                <Row>
-                                    <Col xs={4}>{eq.equation}</Col>
-                                    <Col xs={4}>{eq.confidence}%</Col>
-                                    <Col
-                                        xs={4}
-                                        className="description-col"
-                                        style={{
-                                            flexDirection: eq.expanded
-                                                ? "column"
-                                                : "row"
-                                        }}
-                                    >
-                                        <div
-                                            className={
-                                                eq.expanded
-                                                    ? ""
-                                                    : "ellipses-text"
-                                            }
+                    <MathJax.Context>
+                        <ListGroup variant="flush">
+                            {this.state.equations.map((eq, eq_index) => (
+                                <ListGroup.Item
+                                    key={eq.equation}
+                                    className="equation-list-item"
+                                >
+                                    <Row>
+                                        <Col xs={4}>
+                                            <MathJax.Node>
+                                                {eq.equation}
+                                            </MathJax.Node>
+                                        </Col>
+                                        <Col xs={4}>{eq.confidence}%</Col>
+                                        <Col
+                                            xs={4}
+                                            className="description-col"
+                                            style={{
+                                                flexDirection: eq.expanded
+                                                    ? "column"
+                                                    : "row"
+                                            }}
                                         >
-                                            {eq.description}
-                                        </div>
-                                        <button
-                                            className={`text-button ${
-                                                eq.expanded ? "expanded" : ""
-                                            }`}
-                                            value={eq_index}
-                                            onClick={this.toggle_expand}
-                                        >
-                                            ^
-                                        </button>
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                        ))}
-                    </ListGroup>
+                                            <div
+                                                className={
+                                                    eq.expanded
+                                                        ? ""
+                                                        : "ellipses-text"
+                                                }
+                                            >
+                                                {eq.description}
+                                            </div>
+                                            <button
+                                                className={`expander ${
+                                                    eq.expanded
+                                                        ? "expanded"
+                                                        : ""
+                                                }`}
+                                                value={eq_index}
+                                                onClick={this.toggle_expand}
+                                            >
+                                                <DownCaret
+                                                    height={"1em"}
+                                                    width={"1em"}
+                                                />
+                                            </button>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                            ))}
+                        </ListGroup>
+                    </MathJax.Context>
                 </div>
 
                 <Link to="/">Try different variables</Link>
